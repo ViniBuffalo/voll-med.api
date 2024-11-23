9a98b5e99d4d63bba3029f7509fb151f2c4ed7eb
Vinicius Buffalo <buffalovinicius@gmail.com>
2024-11-22 21:51:53 -0300

    Adicionado Spring Security

--- src/main/resources/db/migration/V4__alter-table-medicos-add-column-ativo.sql
+++ src/main/resources/db/migration/V4__alter-table-medicos-add-column-ativo.sql
@@ -1,3 +1,3 @@
 alter table medicos add column ativo tinyint;
 update medicos set ativo = 1;
-alter table pacientes modify ativo tinyint not null;
+alter table medicos modify ativo tinyint not null;
